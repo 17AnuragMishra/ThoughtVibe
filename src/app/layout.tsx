665fa6aa8c3e83f0e62a87352262313840f00599
17AnuragMishra <anuragmishra0521@gmail.com>
2025-07-21 17:47:56 +0530

    react to next for analytics import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { AuthProvider } from "./contexts/AuthContext";
 import Footer from "./components/Footer";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata: Metadata = {
   title: "ThoughtVibe - Share Your Thoughts",
